5f6dfc7e5e094f63f20d2d30c73f7483fcbfb876
climaxtld <donaldtong@163.com>
2022-03-05 12:58:22 +0800

    Add files via upload

--- src/components/QuickStart.jsx
+++ src/components/QuickStart.jsx
@@ -36,98 +36,56 @@ export default function QuickStart({ isServerInfo }) {
         style={styles.card}
         title={
           <>
-            📝 <Text strong>To-Do List</Text>
+            📝 <Text strong>instruction</Text>
           </>
         }
       >
         <Timeline mode="left" style={styles.timeline}>
           <Timeline.Item dot="📄">
             <Text delete style={styles.text}>
-              Clone or fork{" "}
-              <a
-                href="https://github.com/ethereum-boilerplate/ethereum-boilerplate#-quick-start"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                ethereum-boilerplate
-              </a>{" "}
+              wallet can transfer erc20 token~
             </Text>
           </Timeline.Item>
 
           <Timeline.Item dot="💿">
             <Text delete style={styles.text}>
-              Install all dependencies: <Text code>npm install</Text>
+              Dex can exchange token
             </Text>
           </Timeline.Item>
 
           <Timeline.Item dot="🧰">
             <Text delete={isServerInfo} style={styles.text}>
-              Sign up for a free account on{" "}
-              <a
-                href="https://moralis.io?utm_source=boilerplatehosted&utm_medium=todo&utm_campaign=ethereum-boilerplate"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Moralis
-              </a>
+              NFT can see your
             </Text>
           </Timeline.Item>
 
           <Timeline.Item dot="💾">
             <Text delete={isServerInfo} style={styles.text}>
-              Create a Moralis Server (
-              <a
-                href="https://docs.moralis.io/moralis-server/getting-started/create-a-moralis-server"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                How to start Moralis Server
-              </a>
-              )
+
             </Text>
           </Timeline.Item>
 
           <Timeline.Item dot="🔏">
             <Text delete={isServerInfo} style={styles.text}>
-              Rename <Text code>.env.example</Text> to <Text code>.env</Text>{" "}
-              and provide your <Text strong>appId</Text> and{" "}
-              <Text strong>serverUrl</Text> from{" "}
-              <a
-                href="https://moralis.io?utm_source=boilerplatehosted&utm_medium=todo&utm_campaign=ethereum-boilerplate"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Moralis
-              </a>
-              :
+
             </Text>
             <Text code delete={isServerInfo} style={{ display: "block" }}>
-              REACT_APP_MORALIS_APPLICATION_ID = xxxxxxxxxxxx
+
             </Text>
             <Text code delete={isServerInfo} style={{ display: "block" }}>
-              REACT_APP_MORALIS_SERVER_URL =
-              https://xxxxxx.grandmoralis.com:2053/server
+
             </Text>
           </Timeline.Item>
 
           <Timeline.Item dot="🔁">
             <Text delete={isServerInfo} style={styles.text}>
-              Stop the app and start it again <Text code>npm run start</Text>
+
             </Text>
           </Timeline.Item>
 
           <Timeline.Item dot="💿">
             <Text delete={isInchDex} style={styles.text}>
-              Install{" "}
-              <a
-                href="https://moralis.io/plugins/1inch/?utm_source=boilerplatehosted&utm_medium=todo&utm_campaign=ethereum-boilerplate"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                1inch Moralis Plugin
-              </a>{" "}
-              needed for the<Text code>{"<InchDex />"}</Text> component
-              (optional)
+
             </Text>
           </Timeline.Item>
 
@@ -141,42 +99,24 @@ export default function QuickStart({ isServerInfo }) {
           style={styles.card}
           title={
             <>
-              💣 <Text strong>Starting Local Chain (optional)</Text>
+              💣 <Text strong>Starting Local Chain </Text>
             </>
           }
         >
           <Timeline mode="left" style={styles.timeline}>
             <Timeline.Item dot="💿">
               <Text style={styles.text}>
-                Install{" "}
-                <a
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  href="https://www.npmjs.com/package/truffle"
-                >
-                  Truffle
-                </a>{" "}
-                and{" "}
-                <a
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  href="https://www.npmjs.com/package/ganache-cli"
-                >
-                  ganache-cli
-                </a>{" "}
-                <Text code>npm install -g ganache-cli truffle</Text>
+
               </Text>
             </Timeline.Item>
             <Timeline.Item dot="⚙️">
               <Text style={styles.text}>
-                Start you local devchain: <Text code>npm run devchain</Text> on
-                a new terminal
+                also can Start you local devchain to use it
               </Text>
             </Timeline.Item>
             <Timeline.Item dot="📡">
               <Text style={styles.text}>
-                Deploy test contract: <Text code>npm run deploy</Text> on a new
-                terminal
+                Deploy test contract
               </Text>
             </Timeline.Item>
             <Timeline.Item dot="✅" style={styles.text}>
@@ -191,34 +131,24 @@ export default function QuickStart({ isServerInfo }) {
           title={
             <>
               📡{" "}
-              <Text strong> Connecting your Local Chain to the Moralis DB</Text>
+              <Text strong> Develop by Donald </Text>
             </>
           }
         >
           <Timeline mode="left" style={styles.timeline}>
             <Timeline.Item dot="💿">
               <Text style={styles.text}>
-                Download{" "}
-                <a
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  href="https://github.com/fatedier/frp/releases"
-                >
-                  frpc
-                </a>{" "}
-                and provide missing params in the <Text code>.env</Text> file
+
               </Text>
             </Timeline.Item>
             <Timeline.Item dot="⚙️">
               <Text style={styles.text}>
-                Connect your Moralis Database and Local Chain:{" "}
-                <Text code>npm run connect</Text>
+                this is my first DAPP
               </Text>
             </Timeline.Item>
             <Timeline.Item dot="💾">
               <Text style={styles.text}>
-                Add contract events you want to watch:{" "}
-                <Text code>npm run watch:events</Text>
+                Add contract
               </Text>
             </Timeline.Item>
           </Timeline>
